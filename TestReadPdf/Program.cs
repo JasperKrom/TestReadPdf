c9b74e52d4ad9aea08d642c27d8569bcaf04f615
Jasper Krom <jasper@rapide.software>
2021-09-03 15:14:45 +0200

    Add project files.

---
+++ TestReadPdf/Program.cs
@@ -0,0 +1,17 @@
+﻿using IronPdf;
+using System;
+
+namespace TestReadPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert file location of Pdf File:");
+            var fileLocation = Console.ReadLine();
+            var pdf = new PdfDocument(fileLocation);
+
+            Console.WriteLine($"Security settings edit: {pdf.SecuritySettings.AllowUserEdits}");
+        }
+    }
+}
