add9fbf3bf8910f795e9cccb5a56c069ba5ac85f
Jasper Krom <jasper@rapide.software>
2021-09-06 10:30:22 +0200

    small clarification in comment

--- TestReadPdf/Program.cs
+++ TestReadPdf/Program.cs
@@ -13,6 +13,7 @@ namespace TestReadPdf
 
             Console.WriteLine($"Security settings edit: {pdf.SecuritySettings.AllowUserEdits}");
 
+            //Fails here
             var ForegroundStamp = new IronPdf.Editing.HtmlStamp() { Html = "<h2 style='color:red'>copyright 2018 ironpdf.com", Width = 50, Height = 50, Opacity = 50, Rotation = -45, ZIndex = IronPdf.Editing.HtmlStamp.StampLayer.OnTopOfExistingPDFContent };
             pdf.StampHTML(ForegroundStamp);
         }
